5f8a726043615032d95b16d6b16e1524450e6b8b
Filipe <giovannidelucafelix@gmail.com>
2020-08-17 10:56:03 -0300

    program alterado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ohm{kl
+     class Inter
+     {
+        public static void Main(string[] args)
+        {
+            
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Insira o valor da resistência");
+            calculator.SetRest = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor de voltagem:");
+            calculator.SetVoltage= float.Parse(Console.ReadLine());
+            Console.WriteLine("O valor da corrente é:");
+            Console.WriteLine(calculator.CalculateCorrent() + " ohms");
+            // Retorna o valor da resistência
+            Console.WriteLine(calculator.getRest);
+            // Retorna o valor da voltagem
+            Console.WriteLine(calculator.getVoltage);
+        }
+    }
+}
